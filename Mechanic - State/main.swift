db70ae6c2a9c002b2bb5ee83b5c9451b09bebed3
Reza Shirazian <reza.shirazian@gmail.com>
2016-04-19 00:16:54 -0700

    Initial Commit

---
+++ Mechanic - State/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Mechanic - State
+//
+//  Created by Reza Shirazian on 2016-04-19.
+//  Copyright © 2016 Reza Shirazian. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
